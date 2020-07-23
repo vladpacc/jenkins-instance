cc8c56500769447ffe41c9aa10ce3909a5fcb798
vladpacc <vladpylypenko13@gmail.com>
2020-07-23 16:00:54 -0500

    update

--- deployments/terraform/backend.tf
+++ deployments/terraform/backend.tf
@@ -1,15 +1,6 @@
-#terraform {
- #   backend "s3" {
-   #     bucket = "jenkins-instance-east-1"
-    #    key = "jenkins/us-east-1/class/dev/infrastructure.tfstate"
-     #   region = "us-east-1"
-    #}
-#}
-
-  
 terraform {
     backend "s3" {
-        bucket = "jenkins-instance-east-1"
+        bucket = "jenkins-instance-ec"
         key = "jenkins/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
