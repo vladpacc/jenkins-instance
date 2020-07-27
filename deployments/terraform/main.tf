b409e294678c9e31944870fb3725fb0b09fac3c2
vladpacc <vladpylypenko13@gmail.com>
2020-07-27 14:13:21 -0500

    update

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {}
 resource "aws_instance" "jenkins" {
-  ami           = "var.instance_ami_id"
+  ami           = "${var.instance_ami_id}"
   instance_type = "t2.micro"
   tags = {
-    Name = "var.instance_name"
+    Name = "${var.instance_name}"
   }
 }
 
